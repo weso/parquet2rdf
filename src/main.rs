33850149e0e8e430a5856a7e42f382a75d1407e0
Jose Labra <jelabra@gmail.com>
2023-06-27 08:04:50 +0200

    First version that parses parquet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::env;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let path = &args[1];
+
+    use parquet2::read::read_metadata;
+    let mut reader = std::fs::File::open(path)?;
+    let metadata = read_metadata(&mut reader)?;
+
+    println!("{:#?}", metadata);
+    Ok(())
 }
